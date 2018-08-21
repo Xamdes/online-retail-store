f2b21849e88b3a42ac6c48a93e396c5073fb6870
Steven Colburn <stevencolburn@xamdes.com>
2018-08-21 14:30:07 -0700

    changing to firebase setup

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
 import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 
 @Injectable()
 export class AlbumService
 {
+  albums: AngularFireList<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) {
+    this.albums = database.list('albums');
+  }
 
   getAlbums()
   {
-    return ALBUMS;
+    return this.albums;
   }
 
   getAlbumById(albumId: number)
